094f250265e2c78b7cae6deb2e985ed789862bce
AxelOrtiz21 <bayer_lahm21-1@hotmail.com>
2023-04-22 00:22:31 -0600

    Paso 17 Configurar las rutas

--- LogApp-Gen-1-2023-CDIS-UANL/src/app/app.module.ts
+++ LogApp-Gen-1-2023-CDIS-UANL/src/app/app.module.ts
@@ -2,23 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './components/main/main.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent,
-    RegisterComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
